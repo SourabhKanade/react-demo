a912a82f50200765bc9a1d9ce965ab73aaf31aa0
Sourabh Kanade <kanadesourabh420@gmail.com>
2021-08-07 12:09:06 +0530

    First react app

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <>
+    <h1> Hi, My name is Sourabh Kanade</h1>
+    <p> I am Frontend developer and my tech stacks are as follows: </p>
+    <ol>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>JQuery</li>
+    </ol>
+  </>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
